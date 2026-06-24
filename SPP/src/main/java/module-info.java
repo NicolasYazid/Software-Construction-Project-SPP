40b915dca51128c84c9350bb67bcc096722d3284
Isaac <zS24013258@estudiantes.uv.mx>
2026-06-24 04:13:26 -0600

    Actualiza module-info: abre paquetes coordinador, practicante y profesor a javafx.fxml

--- SPP/src/main/java/module-info.java
+++ SPP/src/main/java/module-info.java
@@ -5,5 +5,8 @@ module mx.uv.spp {
 
     opens mx.uv.spp to javafx.fxml;
     opens mx.uv.spp.controladores.comun to javafx.fxml;
+    opens mx.uv.spp.controladores.coordinador to javafx.fxml;
+    opens mx.uv.spp.controladores.practicante to javafx.fxml;
+    opens mx.uv.spp.controladores.profesor to javafx.fxml;
     exports mx.uv.spp;
 }
