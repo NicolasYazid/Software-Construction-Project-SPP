fd4ceb1d27efc137dc28f5355f22cc7936d7d1ea
NicolasYazid <yazidnicolasch@gmail.com>
2026-06-30 09:50:25 -0600

    Nuevas GUIs del ADMINISTRADOR
    panel principal

--- SPP/src/main/java/module-info.java
+++ SPP/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module mx.uv.spp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.base;
 
     opens mx.uv.spp to javafx.fxml;
     opens mx.uv.spp.controladores.comun to javafx.fxml;
---
+++ SPP/src/main/java/mx/uv/spp/controladores/administrador/PanelAdministradorController.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2026 Nicolás Cruz && Isaac Vazqués.
+ * Todos los derechos reservados.
+ *
+ * Este software es de uso académico y privado.
+ * Fecha de creación: 30 de junio del 2026
+ */
+package mx.uv.spp.controladores.administrador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author yazid
+ */
+public class PanelAdministradorController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
